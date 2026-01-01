85bac2abc39d4fcdf8ed053d9b7219938dcbc13f
Md Jaber Hossain <md9292463@gmail.com>
2026-01-01 22:26:46 +0600

    jj

---
+++ src/components/pages/ProductsDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductsDetails = () => {
+  return (
+  <div className="bg-[url('/src/assets/productsdetailsbg.png']"></div>
+);
+}
+
+export default ProductsDetails
