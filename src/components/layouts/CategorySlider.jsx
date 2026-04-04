0a7d8fb99270a30cf2140dab27971468807a5c98
Sijan666 <666majharulislam@gmail.com>
2026-04-05 02:22:55 +0600

    add responsive in category slider

--- src/components/layouts/CategorySlider.jsx
+++ src/components/layouts/CategorySlider.jsx
@@ -1,32 +1,73 @@
 import Container from "../Container";
 import ProductCS from "../ProductCS";
-// import whiteBg from '../../assets/whiteBg.png'
 import NextArrow from "../NextArrow";
 import PrevArrow from "../PrevArrow";
 import Slider from "react-slick";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-
 const CategorySlider = () => {
-var settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-};
-
     let [allData, setAllData] = useState([]);
+
     useEffect(() => {
         async function alldatas() {
-        let data = await axios.get("https://dummyjson.com/products");
-        setAllData(data.data.products);
+            try {
+                let data = await axios.get("https://dummyjson.com/products");
+                setAllData(data.data.products);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
         }
         alldatas();
-    });
+    }, []);
+
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    };
+
     return (
         <div className="py-25">
             <Container>
@@ -38,16 +79,16 @@ var settings = {
                         29 categories belonging to a total 15,892 products
                     </h6>
                 </div>
-                {/* <div className="flex gap-x-4 items-center"> */}
-                <Slider {...settings}>
-                    {allData.slice(0,10).map((item)=>(
-                        <div className="w-1/4">
-                            <ProductCS csImg={item.thumbnail} csText={item.title}/>
-                        </div>
-                        ))} 
-                    
-                </Slider>
-                {/* </div> */}
+                
+                <div className="slider-container">
+                    <Slider {...settings}>
+                        {allData.slice(0, 10).map((item) => (
+                            <div key={item.id} className="px-2">
+                                <ProductCS csImg={item.thumbnail} csText={item.title} />
+                            </div>
+                        ))}
+                    </Slider>
+                </div>
             </Container>
         </div>
     );
