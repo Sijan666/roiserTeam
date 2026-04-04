586e90ccb69e532f08f138405d55d418c59486ce
Sijan666 <666majharulislam@gmail.com>
2026-04-05 02:36:09 +0600

    add responsive in category collection

--- src/components/layouts/CategoryCollection.jsx
+++ src/components/layouts/CategoryCollection.jsx
@@ -1,21 +1,18 @@
-
 import ButtonforCC from "../ButtonforCC"
 import Container from "../Container"
 import Flex from "../Flex"
-
-
 
 const CategoryCollection = () => {
   return (
     <>
     <Container className={'py-[100px]'}>
-      <Flex className={'gap-x-4'}>
-        <div className="bg-[#E8E8E8] px-10 py-10 rounded-[10px] w-1/2">
+      <Flex className={'gap-x-4 gap-y-4 flex flex-col lg:flex-row'}>
+        <div className="bg-[#E8E8E8] px-10 py-10 rounded-[10px] lg:w-1/2">
           <div className="shadow-newTwo bg-white inline-block px-5 py-3 rounded-[5px]">
             <p className="text-[#E53E3E] font-semibold text-[12px]">1500+ Products</p>
           </div>
           <h4 className="py-5 text-[#1A1A1A] font-semibold text-[24px] uppercase">Women Collections</h4>
-          <p className="w-[390px] text-[#74787C] text-[14px] pb-10">This is genuinely the first theme i bought for which i did not had to write one line of code.</p>
+          <p className="lg:w-[390px] text-[#74787C] text-[14px] pb-10">This is genuinely the first theme i bought for which i did not had to write one line of code.</p>
           <div className="">
             <ButtonforCC btnTexts={'Blazer'} className={'block pb-5 text-base text-[#1A1A1A]'}/>
             <ButtonforCC btnTexts={'Blouses & Shirts'} className={'block pb-5 text-base text-[#1A1A1A]'}/>
@@ -30,7 +27,7 @@ const CategoryCollection = () => {
             <ButtonforCC btnTexts={'Tops & Bodysuits'} className={'block pb-5 text-base text-[#1A1A1A]'}/>
           </div>
         </div>
-        <div className="w-1/2">
+        <div className="lg:w-1/2">
           <div className="bg-[url('/src/assets/cc2.png')] py-10 px-10 rounded-[10px] bg-center bg-cover bg-no-repeat">
             <div className="shadow-newTwo bg-white inline-block px-5 py-3 rounded-[5px]">
               <p className="text-[#E53E3E] font-semibold text-[12px]">1500+ Products</p>
