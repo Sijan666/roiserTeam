5714591725e386a1c9cbfc07fffe399d8fee641e
Majharul Islam <666majharulislam@gmail.com>
2026-04-05 23:57:45 +0600

    change in badges

--- src/components/Badges.jsx
+++ src/components/Badges.jsx
@@ -5,7 +5,6 @@ const Badge = ({ badgesText , className}) => {
       <div className={` px-5 py-1 uppercase font-bold mb-4 w-fit rounded-sm shadow-sm ${className}`}
       style={{
             clipPath: "polygon(0% 0%, 85% 0%, 100% 50%, 85% 100%, 0% 100%)",
-            // clip-[polygon(0_0,100%_0,100%_100%,20px_100%,20px_20px)]
           }}>
         {badgesText}
       </div>
