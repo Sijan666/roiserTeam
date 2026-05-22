c801949e4df7172ba2e415213e23571fbbaebd9a
Sijan666 <666majharulislam@gmail.com>
2026-05-22 22:50:00 +0600

    fix responsive issue error

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -9,15 +9,11 @@ const Error = () => {
     return (
         <>
         <div className="relative">
-            {/* <Images imgSrc={error2} className={'mx-auto mt-[100px]'}/> */}
             <Images imgSrc={error} className={'mx-auto mt-[100px]'}/>
             <div className="errorBtn text-center absolute left-[50%] -translate-x-[50%] -bottom-[110px] ">
                 <Link to={"/"}>
                 <Button btnText={"Back To Homepage"} className={"cursor-pointer text-[#E53E3E]! font-semibold! bg-transparent hover:bg-[#E53E3E]! border border-[#E53E3E]! hover:border-transparent hover:text-white! duration-300 px-[50px] py-[15px]"}/>
                 </Link>
-                {/* <Link to={"/"}>
-                <Button btnText={"Back To Homepage"} className={"cursor-pointer text-[#39AD31]! font-semibold! bg-transparent hover:bg-[#39AD31]! border boder-1 border-[#39AD31]! hover:border-transparent hover:text-white! duration-300"}/>
-                </Link> */}
             </div>
         </div>
         </>
