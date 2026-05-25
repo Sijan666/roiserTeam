66ef49f0ce0ec26d2d3fa0cd0b30b87db88edf23
Sijan666 <666majharulislam@gmail.com>
2026-05-25 23:21:03 +0600

    remove unwanted jsx from app

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,7 @@ import ReactLenis from "lenis/react";
 import ScrollToTop from "./components/ScrollToTop";
 const Home = lazy(() => import("./components/pages/Home"));
 const About = lazy(() => import("./components/pages/About"));
-const Women = lazy(() => import("./components/pages/Women"));
-const MyAccount = lazy(() => import("./components/pages/MyAccount"));
-const Wishlist = lazy(() => import("./components/pages/Wishlist"));
 const Checkout = lazy(() => import("./components/pages/Checkout"));
-const StoreLocation = lazy(() => import("./components/pages/StoreLocation"));
 const ShopGrid = lazy(() => import("./components/pages/ShopGrid"));
 const BlogDetailes = lazy(() => import("./components/pages/BlogDetailes"));
 const Shop_Grid = lazy(() => import("./components/pages/Shop_Grid"));
@@ -34,11 +30,7 @@ function App() {
               <Route path="/" element={<RootLayouts />}>
                 <Route index element={<Home />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/women" element={<Women />} />
-                <Route path="/myaccount" element={<MyAccount />} />
-                <Route path="/wishlist" element={<Wishlist />} />
                 <Route path="/checkout" element={<Checkout />} />
-                <Route path="/storelocation" element={<StoreLocation />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/faq" element={<FAQ />} />
